6af936849ae577dea51a831437600f485bebc712
Aboussejra <70523950+Aboussejra@users.noreply.github.com>
2021-03-26 13:34:40 +0100

    Precision linux how to run in comments

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn Muscu_site:app
--- app/__init__.py
+++ app/__init__.py
@@ -21,3 +21,5 @@ if __name__ == '__main__':
         app.run(host='0.0.0.0')
 # venv\Scripts\activate
 # set FLASK_APP=Muscu_site.py
+
+# Linux : source activate (in venv/bin)
